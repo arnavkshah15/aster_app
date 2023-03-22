7d2c6df733831fe20a31351f29a3a67053621fe5
Arnav Shah <arnavkshah2003@gmail.com>
2023-03-22 22:54:28 +0530

    changed the design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,6 +15,7 @@ List<Itemmodel> plist = [];
 Future<List<Itemmodel>> getPostapi() async {
   final response =
       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
   var data = jsonDecode(response.body.toString());
   if (response.statusCode == 200) {
     plist.clear();
@@ -31,30 +32,127 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.menu),
+      ),
+      backgroundColor: Colors.blue,
       body: Column(children: [
         Expanded(
           child: FutureBuilder(
             future: getPostapi(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
-                Text("Loading");
+                return Center(
+                    child: SizedBox(
+                        width: 48.0,
+                        height: 48.0,
+                        child: CircularProgressIndicator()));
               } else {
                 return ListView.builder(
                     itemCount: plist.length,
                     itemBuilder: (context, index) {
-                      return Container(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Id: ' + plist[index].id.toString()),
-                              Text('UserId: ' + plist[index].userId.toString()),
-                              Text('Title: ' + plist[index].title.toString()),
-                              Text('Body: ' + plist[index].body.toString()),
-                            ],
+                      final color = index.isOdd ? Colors.orange : Colors.green;
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: color,
+                              border: Border.all(style: BorderStyle.none),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'UserId : ',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    Text(
+                                      plist[index].userId.toString(),
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height / 100,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'ID : ',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
+                                    Text(
+                                      plist[index].id.toString(),
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height / 100,
+                                ),
+                                Text(
+                                  'Title :',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  plist[index].title.toString(),
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height / 100,
+                                ),
+                                Text(
+                                  'Body :',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  plist[index].body.toString(),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height / 100,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
